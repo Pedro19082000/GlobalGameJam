74f8cdc9b87a6a9fb93a17cbefb82c96b4ef4856
Guilherme Teotônio <guilhermeteotonio02@gmail.com>
2025-01-24 21:08:44 -0300

    PLAYER FUNCIONANDO APLHA

--- scripts/teste.gd
+++ scripts/teste.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+var teste = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Corpo.apply_central_impulse(Vector2(0, 0))
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(teste)
+func _input(event):
+	# Mouse in viewport coordinates.
+	var mouse = get_viewport( ).get_mouse_position()
+	var player = $Corpo.position
+	var resultado =  mouse - player
+	#print(resultado)
+	var vetor = resultado * -0.02
+	teste = vetor
+	$Corpo.apply_central_impulse(teste)
+	
