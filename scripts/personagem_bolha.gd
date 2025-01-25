d74d08254ef497272660a7b69638135de34cf246
Arthur de Oliveira leite <arthur@oliveiraleite.com.br>
2025-01-25 16:36:13 -0300

    Diminuir e aumentar bolha

--- scripts/personagem_bolha.gd
+++ scripts/personagem_bolha.gd
@@ -3,6 +3,8 @@ extends Node2D
 var teste = Vector2(0, 0)
 var velocidade = -0.05
 
+var pessoas_incial = PlayerVariaveis.pessoas
+
 var entrou = false
 # Called when the node enters the scene tree for the first time.
 
@@ -11,7 +13,16 @@ func _process(delta: float):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and entrou == true:
 		assoprar()
 	PlayerVariaveis.coords = $".".position
+	
+	if pessoas_incial < PlayerVariaveis.pessoas:
+		pessoas_incial = PlayerVariaveis.pessoas
+		aumentar()
+	
 
+func aumentar():
+	$CollisionShape2D.scale = $CollisionShape2D.scale + Vector2(1, 1)
+	$Raio_da_bolha.scale = $Raio_da_bolha.scale + Vector2(0.5,0.5)
+	$Sprite2D.scale = $Sprite2D.scale + Vector2(1, 1)
 func assoprar():
 	var mouse = get_viewport( ).get_mouse_position()
 	var player = $".".position
--- scripts/teste.gd
+++ scripts/teste.gd
@@ -5,3 +5,6 @@ var teste = Vector2(0, 0)
 func _process(delta: float) -> void:
 	$TextEdit.text = "Pessoas: %d" % PlayerVariaveis.pessoas
 	
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+	
