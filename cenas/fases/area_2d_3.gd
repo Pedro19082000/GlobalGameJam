16ea9c07c14e7ad2f82adcd256dba15ee40f103b
Guilherme Teotônio <guilhermeteotonio02@gmail.com>
2025-01-26 15:35:21 -0300

    Fases prontas

--- cenas/fases/area_2d_3.gd
+++ cenas/fases/area_2d_3.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
